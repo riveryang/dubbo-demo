bb0a3354fa0c2d08b0a4e6bb5c22f0db7bd5887a
yanghe <comicme_yanghe@icloud.com>
2016-10-28 15:07:34 +0800

    修改模块名autoscan为annotation

--- dubbo-demo-provider-with-annotation/src/main/java/org/dubbo/demo/service/impl/HelloWorldServiceImpl.java
+++ dubbo-demo-provider-with-annotation/src/main/java/org/dubbo/demo/service/impl/HelloWorldServiceImpl.java
@@ -4,7 +4,7 @@ import org.dubbo.demo.service.HelloWorldService;
 
 import com.alibaba.dubbo.config.annotation.Service;
 
-@Service
+@Service(version = "0.0.1", owner = "yanghe", group = "default", loadbalance = "random", timeout = 5, retries = 2, token = "true", weight = 100)
 public class HelloWorldServiceImpl implements HelloWorldService {
 
 	@Override
