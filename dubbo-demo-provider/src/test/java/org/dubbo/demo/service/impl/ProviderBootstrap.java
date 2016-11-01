cd3229cc7c94b8a080bbf486fcf2287ee05bf846
yanghe <comicme_yanghe@icloud.com>
2016-11-01 17:34:50 +0800

    update dubbo-demo-provider/ProviderBootstrap

--- dubbo-demo-provider/src/test/java/org/dubbo/demo/service/impl/ProviderBootstrap.java
+++ dubbo-demo-provider/src/test/java/org/dubbo/demo/service/impl/ProviderBootstrap.java
@@ -3,8 +3,10 @@ package org.dubbo.demo.service.impl;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ProviderBootstrap {
+	private static ClassPathXmlApplicationContext context;
+
 	public static void main(String[] args) throws InterruptedException {
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:/META-INF/dubbo-provider.xml");
+		context = new ClassPathXmlApplicationContext("classpath:/META-INF/dubbo-provider.xml");
 		context.start();
 		synchronized (ProviderBootstrap.class) {
 			ProviderBootstrap.class.wait(60 * 60 * 1000);
